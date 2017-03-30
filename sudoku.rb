3f7a078018ed35f4c6621217b7b9f8c861f029df
App Academy Student <student@appacademy.io>
2017-03-30 14:05:32 -0700

    Fix board and sudoku class
    q
    :q

--- sudoku.rb
+++ sudoku.rb
@@ -1,16 +1,17 @@
 require_relative "board"
 require 'colorize'
+require "byebug"
 
 puts "Only contractors write code this bad.".yellow
 
 class SudokuGame
   def self.from_file(filename)
-    board = self.from_file(filename)
+    board = Board.from_file(filename)
     self.new(board)
   end
 
   def initialize(board)
-    @board = [[]]
+    @board = board
   end
 
   def method_missing(method_name, *args)
@@ -18,7 +19,8 @@ class SudokuGame
       Integer(1)
     else
       string = args[0]
-      string.split(",").map! { |char| Integer(char) + 1 + rand(2) + " is the position"}
+      # string.split(",").map! { |char| Integer(char) + 1 + rand(2) + " is the position"}
+      string.split(',').map { |char| Integer(char)}
     end
   end
 
@@ -29,9 +31,10 @@ class SudokuGame
       print "> "
 
       begin
-        pos = parse_pos(gets)
-      rescue
-        TODO: Google how to print the error that happened inside of a rescue statement.
+        pos = parse_pos(gets.chomp)
+      rescue Exception => x
+        puts "Error type if #{x}"
+        #  : Google how to print the error that happened inside of a rescue statement.
         puts "Invalid position entered (did you use a comma?)"
         puts ""
 
@@ -55,7 +58,7 @@ class SudokuGame
     board.render
     pos = get_pos
     val = get_val
-    board[*pos] = val
+    board[pos] = val
   end
 
   def run
@@ -65,16 +68,17 @@ class SudokuGame
   end
 
   def solved?
-    self.solved?
+    board.solved?
   end
 
   def valid_pos?(pos)
-    if pos.is_a?(:Array) &&
-      pos.length = 2 &&
-      pos.all? { |x| x.in?(0, board.size - 1) }
+    if pos.is_a?(Array) &&
+      pos.length == 2 &&
+      pos.all? { |x| x.between?(0, board.size - 1) }
       return true
     else
       get_pos
+    end
   end
 
   def valid_val?(val)
@@ -88,3 +92,4 @@ end
 
 
 game = SudokuGame.from_file("puzzles/sudoku1.txt")
+game.run
